9fd09678992b0b4d5257bef0365a49e29d0f6ce7
OmerYa <omeryairmail@gmail.com>
2016-10-05 12:58:41 -0600

    Fix setup.bat

--- Mario/Release/Setup.bat
+++ Mario/Release/Setup.bat
@@ -1,4 +1,6 @@
 @echo off
-copy *.* %windir%\System32
+copy EasyHook32.dll %windir%\System32
+copy Mario.dll %windir%\System32
 reg import Mario.reg
 echo Done, please reboot the machine to complete the installation 
+pause
